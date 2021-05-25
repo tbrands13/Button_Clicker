9011d5fcd158d84c8e285a2684662de67a20c277
Titus Brandy <tbrands13@gmail.com>
2021-05-24 19:42:36 -0500

    INIT COMMIT

---
+++ script.js
@@ -0,0 +1,10 @@
+// var clickHeader = document.getElementById('clickMe').addEventListener("click"), function()
+
+
+function headerAlert() {
+    alert("Once upon a time ago in a galaxy far far away...");
+}
+
+function winPrint() {
+    window.print();
+}
